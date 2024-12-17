4db10c71de694185aa9c2ecf1c23fa50a607ceba
tharunamireddy <108273258+tharunamireddy@users.noreply.github.com>
2024-12-17 15:13:57 +0530

    Update page.tsx

--- src/components/TimeLine/page.tsx
+++ src/components/TimeLine/page.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 import React from "react";
 import { Timeline } from "@/components/ui/timeline";
 import {
+  ed1,
+  ed2,
+  ed3,
+  ed4,
   bw1,
   bw2,
   bw3,
@@ -55,33 +59,33 @@ export function TimelineSection() {
             <br />
             <strong>Website Launch Setup:</strong> Codehub Website has been launched by our Principal, gave valuable insights on engineers and their importance 
             <br />
-            <strong>Event Conclusion:</strong> The event concluded with a rapid-fire with Alumni, a day in their work space.
+            <strong>Event Conclusion:</strong> The event concluded with a rapid-fire with Alumni, a day in their workspace.
           </p>
 
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src={bw1}
+              src={ed1}
               alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src={bw2}
+              src={ed2}
               alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src={bw3}
+              src={ed3}
               alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src={bw4}
+              src={ed4}
               alt="startup template"
               width={500}
               height={500}
